157c7c69a2d5e13cc75d45098e0102eefdd1b722
Maringmei <mshengangkabui@gmail.com>
2023-03-02 12:46:29 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/firestorecrud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firestorecrud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'Screen/home_page.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
